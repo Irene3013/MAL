3322b518c9ff3b38d515dc46ddb0e20063c9eea9
Irene3013 <i.etxag@gmail.com>
2025-12-29 13:39:53 +0100

    UPDATE env

--- clip_flant5_vsr.sh
+++ clip_flant5_vsr.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-user=irene.etxarri@ehu.eus
 
 #source /gscratch/users/asalaberria009/env/p39-cu115/bin/activate
-source /gaueko0/users/ietxarri010/env/t2v_metrics_venv/bin/activate
+source /gaueko0/users/ietxarri010/env/t2v_metrics_py39/bin/activate
 
 #export TRANSFORMERS_CACHE="/ncache/hub/"
 
