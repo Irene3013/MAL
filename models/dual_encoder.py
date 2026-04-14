14069e255914b9f52bcf6fd31b96262ff0b7bf96
Irene3013 <i.etxag@gmail.com>
2026-04-14 12:38:57 +0200

    UPDATE clean code

--- models/dual_encoder.py
+++ models/dual_encoder.py
@@ -26,9 +26,7 @@ class DualEncoder(pl.LightningModule):
         self.beta_2 = args.beta_2
         self.eps = args.eps
         self.weight_decay = args.weight_decay
-
         self.scheduler_off = args.scheduler_off
-        self.batch_size = args.batch_size
         self.cross_entropy = torch.nn.CrossEntropyLoss()
         self.score = args.score
 
@@ -42,11 +40,6 @@ class DualEncoder(pl.LightningModule):
     # STEP (train/val/test)
     # -----------------------------
     def step(self, batch, split):
-        # if split == "train":
-        #     return self.train_step(batch, split)
-        # else:
-        #     return self.eval_step(batch, split)
-
         inputs = self.move_to_device(batch, self.device)
 
         if split == "train":
@@ -56,7 +49,7 @@ class DualEncoder(pl.LightningModule):
             logits = logits_per_image
 
             N = logits_per_image.shape[0]
-            N_pairs = logits_per_image.shape[0]/2
+            N_pairs = logits_per_image.shape[0] // 2
             ground_truth = torch.arange(N, device=self.device)        
             loss = 0.5 * (
                 self.cross_entropy(logits_per_image, ground_truth) +
@@ -97,60 +90,6 @@ class DualEncoder(pl.LightningModule):
         # Logging
         self.log(f'{split}_accuracy', acc, on_epoch=True, prog_bar=(split=="train"), logger=True, batch_size=N_pairs)
         return loss if split == 'train' else acc
-        
-        
-    # def train_step(self, batch, split):
-    #     inputs = self.move_to_device(batch, self.device)
-
-    #     outputs = self.model(**inputs)
-    #     logits_per_image = outputs.logits_per_image
-    #     logits_per_text = outputs.logits_per_text
-
-    #     ground_truth = torch.arange(2*self.batch_size, device=self.device)        
-    #     loss = 0.5 * (
-    #         self.cross_entropy(logits_per_image, ground_truth) +
-    #         self.cross_entropy(logits_per_text, ground_truth)
-    #     )
-
-    #     self.log(f'{split}_loss', loss, batch_size=self.batch_size)
-    #     return loss
-    
-       
-    # def eval_step(self, batch, split): 
-    #     inputs = self.move_to_device(batch, self.device)
-
-    #     if self.model_name == "pecore":
-    #         image_features, text_features, logit_scale = self.model(inputs["image"], inputs["captions"])
-    #         logits = logit_scale * image_features @ text_features.T
-    #     else:
-    #         outputs = self.model(**inputs)
-    #         logits = outputs.logits_per_image
-        
-    #     acc = 0 # Group score per each pair
-    #     for i in range(self.batch_size):
-    #         start = 2 * i
-    #         end   = 2 * i + 2
-    #         sub = logits[start:end, start:end]
-            
-    #         #        TexPos TexNeg
-    #         # ImgPos   a      b
-    #         # ImgNeg   c      d
-    #         a, b = sub[0, 0], sub[0, 1] 
-    #         c, d = sub[1, 0], sub[1, 1] 
-
-    #         Ipos_2T = (a > c).item()
-    #         Tpos_2I = (a > b).item()
-    #         Ineg_2T = (d > b).item()
-    #         Tneg_2I = (d > c).item()
-
-    #         group_score = Ipos_2T and Ineg_2T and Tpos_2I and Tneg_2I
-    #         acc += int(group_score)
-
-    #     acc /= self.batch_size
-
-    #     # Logging
-    #     self.log(f'{split}_accuracy', acc, on_epoch=True, prog_bar=(split=="train"), logger=True, batch_size=self.batch_size)
-    #     return acc
     
 
     def move_to_device(self, batch, device):
