ff7c0efe2f878efc8fcf67ad8bdda9fbcedde2ac
Irene3013 <i.etxag@gmail.com>
2026-05-05 09:38:59 +0200

    UPDATE .sh

--- exec/pecore_zeroshot_mm.sh
+++ exec/pecore_zeroshot_mm.sh
@@ -15,8 +15,8 @@ source /gaueko0/users/ietxarri010/env/pe_core/bin/activate
 
 
 srun python main.py --model "pecore" \
-   --lr 1e-6 --batch_size 50 --max_steps 10000 --accumulate_grad_batches 1 \
-   --run_name E0 --train --evaluate --dataset rel \
+   --lr 1e-6 --batch_size 200 --max_steps 1000 --accumulate_grad_batches 1 \
+   --run_name proba_E0 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v1
 
 # srun python main.py --model "pecore" \
