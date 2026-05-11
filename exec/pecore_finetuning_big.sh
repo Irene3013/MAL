a7067866489a182e456deb499ee34d1fdf28be95
Irene3013 <i.etxag@gmail.com>
2026-05-11 10:57:54 +0200

    UPDATE v1v2

--- exec/pecore_finetuning_big.sh
+++ exec/pecore_finetuning_big.sh
@@ -19,10 +19,10 @@ srun python main.py --model "pecore" \
    --run_name proba_E0 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v1
 
-# srun python main.py --model "pecore" \
-#    --lr 1e-6 --batch_size 200 --max_steps 5000 --accumulate_grad_batches 1 \
-#    --run_name E0 --train --evaluate --dataset rel \
-#    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v2
+srun python main.py --model "pecore" \
+   --lr 1e-6 --batch_size 200 --max_steps 5000 --accumulate_grad_batches 1 \
+   --run_name E0 --train --evaluate --dataset rel \
+   --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v2
 
 # srun python main.py --model "pecore" \
 #    --lr 1e-6 --batch_size 200 --max_steps 5000 --accumulate_grad_batches 1 \
