6fc48eb0ceca9553d7840146c0653cc69f989acf
Irene3013 <i.etxag@gmail.com>
2026-05-19 10:35:34 +0200

    UPDATE pecore bashrc

--- exec/pecore_finetuning_small.sh
+++ exec/pecore_finetuning_small.sh
@@ -48,10 +48,14 @@ source /gaueko0/users/ietxarri010/env/pe_core/bin/activate
 #    --lr 1e-6 --batch_size 2 --max_steps 5000 --accumulate_grad_batches 1 \
 #    --run_name E5 --train --evaluate --dataset rel \
 #    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v7
+srun python main.py --model "pecore" \
+   --lr 1e-6 --batch_size 2 --max_steps 5000 --accumulate_grad_batches 1 \
+   --run_name pecore_zeroshot --evaluate --dataset rel \
+   --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v9
 
 srun python main.py --model "pecore" \
    --lr 1e-6 --batch_size 2 --max_steps 5000 --accumulate_grad_batches 1 \
-   --run_name E5 --train --evaluate --dataset rel \
+   --run_name pecore_E5 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v9
 
 
@@ -93,7 +97,7 @@ srun python main.py --model "pecore" \
 
 srun python main.py --model "pecore" \
    --lr 1e-6 --batch_size 4 --max_steps 5000 --accumulate_grad_batches 1 \
-   --run_name E4 --train --evaluate --dataset rel \
+   --run_name pecore_E4 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v9
 
 # BATCH 16
@@ -134,7 +138,7 @@ srun python main.py --model "pecore" \
 
 srun python main.py --model "pecore" \
    --lr 1e-6 --batch_size 16 --max_steps 5000 --accumulate_grad_batches 1 \
-   --run_name E3 --train --evaluate --dataset rel \
+   --run_name pecore_E3 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v9
 
 # BATCH 64
@@ -175,5 +179,5 @@ srun python main.py --model "pecore" \
 
 srun python main.py --model "pecore" \
    --lr 1e-6 --batch_size 64 --max_steps 5000 --accumulate_grad_batches 1 \
-   --run_name E2 --train --evaluate --dataset rel \
+   --run_name pecore_E2 --train --evaluate --dataset rel \
    --root /gaueko0/users/ietxarri010/MAL/data/raw/RelationsDataset --precision 32 --variant v9
